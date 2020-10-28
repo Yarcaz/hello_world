99884d0b1cf81c7124d8a13691b70556e2a2344b
Yarcaz <Yarcaz@gmail.com>
2020-10-28 12:28:31 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,10 +5,12 @@ public class hello {
     public static void main(String [] args) {
 
         // initialize variables
-        String firstName = "Alex";
+        //String firstName = "Alex";
+		String firstName = "Zachary";
 	
 		// output welcome message
-        System.out.println("Hello world!");
+		//System.out.println("Hello world!");
+        System.out.println("Hello, my name is " + firstName);
 
 
     }
